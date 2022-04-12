a46cb25984216c87ec56c108cc0b803a3fb18818
munjeongmin <3330kys@dsm.hs.kr>
2022-04-12 23:14:18 +0900

    [EDIT] 컨트롤러 설정

--- Test/src/main/java/com/example/test/domain/user/api/UserController.java
+++ Test/src/main/java/com/example/test/domain/user/api/UserController.java
@@ -1,16 +1,23 @@
 package com.example.test.domain.user.api;
 
+import com.example.test.domain.user.application.UserServicelmpl;
+import com.example.test.domain.user.dto.request.CreateUserRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+
+    private final UserServicelmpl userServicelmpl;
+
+    @ResponseStatus(HttpStatus.CREATED)
     @GetMapping("/login")
-    public String login () {
-        return "login!!";
+    public String login (@RequestBody @Valid CreateUserRequest request) {
+        return userServicelmpl.createUser(request);
     }
 }
