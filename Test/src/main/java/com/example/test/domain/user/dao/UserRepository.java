0d33771ecfc7630f80b60df9e38724bf6b82f58b
munjeongmin <3330kys@dsm.hs.kr>
2022-04-13 23:00:34 +0900

    [EDIT] findByAccountId repository에 등록

--- Test/src/main/java/com/example/test/domain/user/dao/UserRepository.java
+++ Test/src/main/java/com/example/test/domain/user/dao/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.test.domain.user.dao;
 import com.example.test.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByAccountId(String accountId);
 }
