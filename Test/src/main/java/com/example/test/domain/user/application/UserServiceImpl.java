bf7681380ce55091e2a7c9d0c034bde7f6cf192d
munjeongmin <3330kys@dsm.hs.kr>
2022-04-14 14:43:57 +0900

    [ADD] 로그인 시 해시된 비밀번호 검사 로직 추가

--- Test/src/main/java/com/example/test/domain/user/application/UserServiceImpl.java
+++ Test/src/main/java/com/example/test/domain/user/application/UserServiceImpl.java
@@ -4,22 +4,20 @@ import com.example.test.domain.user.dao.UserRepository;
 import com.example.test.domain.user.domain.User;
 import com.example.test.domain.user.dto.request.CreateUserRequest;
 import com.example.test.domain.user.dto.request.SigninRequest;
+import com.example.test.domain.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
-public class UserServicelmpl implements UserService {
+public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Override
-    @Transactional
     public String signup(CreateUserRequest request) {
 
         userRepository.save(User.builder()
@@ -31,12 +29,16 @@ public class UserServicelmpl implements UserService {
     }
 
     @Override
-    @Transactional
-    public String signin(SigninRequest request) {
-        Optional<User> user = userRepository.findByAccountId(request.getAccountId());
+    @Transactional(readOnly = true)
+    public String login(SigninRequest request) {
+        User user = userRepository.findByAccountId(request.getAccountId())
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
 
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword()))
+            return "false";
 
-
-        return "login success!!!";
+        return "success!";
     }
+
+
 }
