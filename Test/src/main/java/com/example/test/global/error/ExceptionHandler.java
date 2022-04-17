598b4205e2aa731f22a7aaabb4fcddb294b8dfdc
munjeongmin <3330kys@dsm.hs.kr>
2022-04-17 23:43:47 +0900

    [ADD] exceptionFilter exception 필터추가

---
+++ Test/src/main/java/com/example/test/global/error/ExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.test.global.error;
+
+import com.example.test.global.error.exception.TestException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ExceptionHandler extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            filterChain.doFilter(request,response);
+        } catch (TestException e) {
+            ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getMessage());
+
+            response.setStatus(e.getErrorCode().getStatus());
+            response.setContentType("application/json");
+            response.getWriter().write(errorResponse.convertToJson(errorResponse));
+        }
+    }
+}
