448fac14607d78a1ac7e0f4182be2c7713452092
munjeongmin <3330kys@dsm.hs.kr>
2022-04-18 13:27:39 +0900

    [ADD] refreshTokenRepository 추가

---
+++ Test/src/main/java/com/example/test/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -0,0 +1,11 @@
+package com.example.test.domain.auth.domain.repository;
+
+import com.example.test.domain.auth.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
+}
