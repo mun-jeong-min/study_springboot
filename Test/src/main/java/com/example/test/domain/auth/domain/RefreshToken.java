09bc55d920ae9075fffdee0f823ebcf73406db32
munjeongmin <3330kys@dsm.hs.kr>
2022-04-18 13:27:06 +0900

    [ADD] refreshtoken entity 추가

---
+++ Test/src/main/java/com/example/test/domain/auth/domain/RefreshToken.java
@@ -0,0 +1,24 @@
+package com.example.test.domain.auth.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@Builder
+@RedisHash
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class RefreshToken {
+
+    @Id
+    private String accountId;
+
+    @Indexed
+    private String token;
+
+    @TimeToLive
+    private Long timeToLive;
+}
