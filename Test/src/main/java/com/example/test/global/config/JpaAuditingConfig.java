49fc0ab94c4c05eba558e839a926dbd210f20b2b
munjeongmin <3330kys@dsm.hs.kr>
2022-04-19 21:45:32 +0900

    [ADD] Jpaudting 추가

---
+++ Test/src/main/java/com/example/test/global/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.example.test.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configurations
+public class JpaAuditingConfig {
+}
