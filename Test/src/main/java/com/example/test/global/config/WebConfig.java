71d265f400722d4313354086aa5c1da8d0bcac01
munjeongmin <3330kys@dsm.hs.kr>
2022-04-19 11:28:57 +0900

    [ADD]webMvcConfig 추가

---
+++ Test/src/main/java/com/example/test/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.test.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowedOrigins("*")
+                .allowedHeaders("*");
+    }
+}
