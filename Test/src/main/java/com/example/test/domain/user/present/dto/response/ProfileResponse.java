e47658bc5ce536750defb7e9e03d3c30375ec5c9
munjeongmin <3330kys@dsm.hs.kr>
2022-04-20 22:47:02 +0900

    [ADD] profileResponse 추가

---
+++ Test/src/main/java/com/example/test/domain/user/present/dto/response/ProfileResponse.java
@@ -0,0 +1,10 @@
+package com.example.test.domain.user.present.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ProfileResponse {
+    private String username;
+}
