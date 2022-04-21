b23464aca7e296db433aafb99e8bcadbf427b592
munjeongmin <3330kys@dsm.hs.kr>
2022-04-21 17:27:05 +0900

    [ADD] boardCreate controller 추가

--- demo/src/main/java/com/example/demo/board/present/BoardController.java
+++ demo/src/main/java/com/example/demo/board/present/BoardController.java
@@ -1,11 +1,21 @@
 package com.example.demo.board.present;
 
+import com.example.demo.board.present.dto.request.BoardCreateRequest;
+import com.example.demo.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
+
+    private final BoardService boardService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/create")
+    public void boardCreate(@RequestBody BoardCreateRequest request) {
+        boardService.boardCreate(request);
+    }
 }
