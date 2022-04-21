f7d8b945f9841304e668fcd41807bd5dcf790442
munjeongmin <3330kys@dsm.hs.kr>
2022-04-21 17:26:52 +0900

    [ADD] boardService Create 추가

--- demo/src/main/java/com/example/demo/board/service/BoardService.java
+++ demo/src/main/java/com/example/demo/board/service/BoardService.java
@@ -1,10 +1,24 @@
 package com.example.demo.board.service;
 
+import com.example.demo.board.domain.Board;
+import com.example.demo.board.domain.repository.BoardRepository;
+import com.example.demo.board.present.dto.request.BoardCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
-    
+
+    private final BoardRepository boardRepository;
+
+    public void boardCreate(BoardCreateRequest request) {
+        boardRepository.save(
+                Board.builder()
+                        .title(request.getTitle())
+                        .description(request.getDescription())
+                        .subTitle(request.getSubtitle())
+                        .build()
+        );
+    }
 }
