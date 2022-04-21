c9ba3848099fe7dd9486a80674040a5681ba13e0
munjeongmin <3330kys@dsm.hs.kr>
2022-04-21 20:03:37 +0900

    [ADD] exceptionHandler 추가

---
+++ demo/src/main/java/com/example/demo/global/error/ExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.demo.global.error;
+
+import com.example.demo.global.error.exception.DemoException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ExceptionHandler extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(request,response);
+        } catch (DemoException e) {
+            e.printStackTrace(); // 배포 전에 에러 띄우는 용
+            ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode().getMessage());
+
+            response.setStatus(e.getErrorCode().getStatus());
+            response.setContentType("application/json");
+            response.getWriter().write(errorResponse.convertJson(errorResponse));
+        }
+    }
+}
