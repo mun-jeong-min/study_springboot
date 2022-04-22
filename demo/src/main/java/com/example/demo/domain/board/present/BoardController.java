89c558ee51a0e7c9acbf887509e9f48b08fef81e
munjeongmin <3330kys@dsm.hs.kr>
2022-04-22 13:50:05 +0900

    [EDIT] controller 에 boardAll 추가

--- demo/src/main/java/com/example/demo/domain/board/present/BoardController.java
+++ demo/src/main/java/com/example/demo/domain/board/present/BoardController.java
@@ -1,10 +1,15 @@
 package com.example.demo.domain.board.present;
 
+import com.example.demo.domain.board.domain.Board;
 import com.example.demo.domain.board.present.dto.request.BoardCreateRequest;
+import com.example.demo.domain.board.present.dto.request.BoardUpdateRequest;
+import com.example.demo.domain.board.present.dto.response.BoardReadResponse;
 import com.example.demo.domain.board.service.BoardServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +22,28 @@ public class BoardController {
     @PostMapping("/create")
     public void boardCreate(@RequestBody BoardCreateRequest request) {
         boardService.boardCreate(request);
+    }
+/*
+    @GetMapping("/read/{id}")
+    public BoardReadResponse boardRead(@PathVariable("id") Long id) {
+        return boardService.boardRead(id);
+    }
+    */
+
+    @GetMapping("/read")
+    public List<Board> boardAll() {
+        return boardService.boardAll();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/update/{id}")
+    public void boardUpdate(@RequestBody BoardUpdateRequest request, @PathVariable("id") Long id) {
+        boardService.boardUpdate(request, id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/delete/{id}")
+    public void boardDelete(@PathVariable("id") Long id) {
+        boardService.boardDelete(id);
     }
 }
